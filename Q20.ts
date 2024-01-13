2a78e5208a45296c535950d041516bb6a4762854
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 03:28:00 +0500

    NODEJS Assignment Q20
    20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these item

---
+++ Q20.ts
@@ -0,0 +1,5 @@
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+ 
+ let proglanguages:string[] =[
+    "HTML", "CSS","javaScript/TypeScript","php"];
+    proglanguages.map((item) => console.log(item));
